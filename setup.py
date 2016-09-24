08ae953eb4453465b4befacfd8abd80da0906c53
Kenny Do <kedo@patreon.com>
2016-09-24 02:34:17 -0700

    Set up python project skeleton

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='meiling',
+    version='0.0.1',
+    description="Authenticate nginx requests through OAuth",
+    url='https://github.com/kennydo/nginx-meiling-proxy',
+    author='Kenny Do',
+    author_email='chinesedewey@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+    ],
+    packages=[
+        'meiling',
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
