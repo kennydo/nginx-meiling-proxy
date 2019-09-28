3e47138986413d75f1dbba56170e0a701a547514
Kenny Do <chinesedewey@gmail.com>
2019-09-27 23:12:54 -0700

    Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
 
 setup(
     name='meiling',
-    version='0.0.5',
+    version='0.0.6',
     description="Authenticate nginx requests through OAuth",
     url='https://github.com/kennydo/nginx-meiling-proxy',
     author='Kenny Do',
